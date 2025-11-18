3aed3c874d0dd542684bf67288827cddfcb66954
KogarkoP <kogarkopavel@gmail.com>
2025-11-18 14:01:42 +0200

    Fixed mistake in userUpdateSchema

--- server/src/schemas/userUpdate.js
+++ server/src/schemas/userUpdate.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const userUpdateSchema = Joi.object({
   userOption: Joi.string().required(),
-  userValue: Joi.string().required(),
+  userValue: Joi.alternatives().try(Joi.string(), Joi.boolean()).required(),
 });
 
 export default userUpdateSchema;
